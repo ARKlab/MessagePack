7040a2fdcc756225ed67782f841ff4f441d9110e
adam-murphy <adam.murphy@ark-energy.eu>
2018-05-14 11:38:43 +0100

    More complex periods tested

--- MessagePack/MessagePack.NodaTime.Tests/NodaTimeTests/PeriodMessagePackFormatterTest.cs
+++ MessagePack/MessagePack.NodaTime.Tests/NodaTimeTests/PeriodMessagePackFormatterTest.cs
@@ -3,6 +3,7 @@
 // license information. 
 using MessagePack.NodaTime.Tests.Helpers;
 using NodaTime;
+using System;
 using Xunit;
 
 namespace MessagePack.NodaTime.Tests
@@ -15,19 +16,41 @@ namespace MessagePack.NodaTime.Tests
         {
             Period p = Period.FromDays(1);
             Assert.Equal(TestTools.Convert(p), p);
-        }        
+        }
 
         [Fact]
         public void PeriodArrayTest()
         {
+            var pp = new PeriodBuilder
+            {
+                Years = DateTime.UtcNow.Year,
+                Months = DateTime.UtcNow.Month,
+                Weeks = 4,
+                Days = DateTime.UtcNow.Day,
+                Hours = new DateTime().Hour,
+                Minutes = DateTime.UtcNow.Minute,
+                Seconds = new DateTime().Second,
+                Milliseconds = DateTime.UtcNow.Millisecond,
+                Ticks = DateTime.Now.Ticks,
+                Nanoseconds = DateTime.UtcNow.Ticks / 100,
+            }.Build();
+
+            var pp1 = new PeriodBuilder
+            {
+                Years = DateTime.UtcNow.Year,
+                Months = DateTime.UtcNow.Month,
+                Weeks = 4,
+                Days = new DateTime().Day               
+            }.Build();
+
             Period[] p = new Period[]
-                { Period.FromDays(1),
-                Period.FromMonths(2),
+                { Period.FromYears(1),
+                pp,
                 Period.FromDays(1),
                 Period.FromNanoseconds(5),
-                Period.FromHours(3)
+                pp1
             };
             Assert.Equal(TestTools.Convert(p), p);
         }
-     }
+    }
 }
