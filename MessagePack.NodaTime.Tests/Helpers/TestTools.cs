e79afb850a10e5c9cc507738a577022e8fa21e12
adam-murphy <adam.murphy@ark-energy.eu>
2018-05-29 16:48:34 +0100

    Solution/Folder restructure

---
+++ MessagePack.NodaTime.Tests/Helpers/TestTools.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) ARK LTD. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for
+// license information. 
+namespace MessagePack.NodaTime.Tests.Helpers
+{
+    static class TestTools
+    {
+        public static T Convert<T>(T value)
+        {
+            return MessagePackSerializer.Deserialize<T>(MessagePackSerializer.Serialize(value));
+        }
+    }
+}
---
+++ MessagePack.NodaTime.Tests/NodaTimeTests/LocalTimeMessagePackTest.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) ARK LTD. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for
+// license information. 
+using MessagePack.NodaTime.Tests.Helpers;
+using NodaTime;
+using Xunit;
+
+namespace MessagePack.NodaTime.Tests
+{
+    [Collection("ResolverCollection")]
+    public class LocalTimeMessagePackFormatterTest
+    {
+        [Fact]
+        public void LocalTimeTest()
+        {
+            LocalTime t = LocalTime.FromSecondsSinceMidnight(1);
+            Assert.Equal(TestTools.Convert(t), t);
+        }
+
+        [Fact]
+        public void NullableLocalTimeTest()
+        {
+            LocalTime? t = null;
+            Assert.Equal(TestTools.Convert(t), t);
+        }        
+
+        [Fact]
+        public void LocalTimeArrayTest()
+        {
+            LocalTime[] lt =
+                { LocalTime.FromTicksSinceMidnight(4000),
+                LocalTime.FromSecondsSinceMidnight(10000),
+                LocalTime.FromHourMinuteSecondTick(20,10,1,13),
+                new LocalTime(),
+                LocalTime.FromSecondsSinceMidnight(1)
+            };
+            Assert.Equal(TestTools.Convert(lt), lt);
+        }
+
+        [Fact]
+        public void NullableLocalTimeArrayTest()
+        {
+            LocalTime?[] lt = new LocalTime?[] {
+                null,
+                null,
+                null,
+                null,
+                null
+            };
+            Assert.Equal(TestTools.Convert(lt), lt);
+        }        
+    }
+}
